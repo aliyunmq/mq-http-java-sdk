9696733cbff349a07b8aaa5fd8209812737544f2
aliyunmq <xigu.lx@taobao.com>
2020-01-15 10:11:29 +0800

    upgrade log4j to log4j2

--- src/main/java/com/aliyun/mq/http/common/utils/Utils.java
+++ src/main/java/com/aliyun/mq/http/common/utils/Utils.java
@@ -1,13 +1,14 @@
 package com.aliyun.mq.http.common.utils;
 
 import com.aliyun.mq.http.common.Constants;
-import org.apache.log4j.Logger;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 
 public class Utils {
-    public static Logger logger = Logger.getLogger(Utils.class);
+    public static Log logger = LogFactory.getLog(Utils.class);
 
     public static URI getHttpURI(String endpoint) {
         if (endpoint == null) {
@@ -17,7 +18,7 @@ public class Utils {
 
         try {
             if (!endpoint.startsWith(Constants.HTTP_PREFIX) && !endpoint.startsWith(Constants.HTTPS_PREFIX)) {
-                logger.warn("Only support http or https protocol。Endpoint must be started by http:// or https://.");
+                logger.warn("Only support http or https protocol.Endpoint must be started by http:// or https://.");
                 throw new IllegalArgumentException("Only support http or https protocol。Endpoint must be started by http:// or https://.");
             }
             while (endpoint.endsWith(Constants.SLASH)) {
